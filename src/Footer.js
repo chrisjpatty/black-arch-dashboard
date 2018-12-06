4bafc08dac6f8a3148e0cddd4631a211e4e4060c
Christopher Patty <christopher.jon.patty@gmail.com>
2018-12-06 00:38:05 -0700

    Added disclaimer

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'react-emotion'
 import firebase from './api'
 
 const Footer = () => {
-  const logout = () => {
-    firebase.auth().signOut()
-  }
-
+  const [disclaimerShown, setDisclaimerShown] = useState(false)
+  const logout = () => firebase.auth().signOut()
+  const toggleDisclaimer = () => setDisclaimerShown(!disclaimerShown)
   return (
     <Wrapper>
+      {disclaimerShown && <Disclaimer onDismiss={toggleDisclaimer} />}
+      <Button onClick={toggleDisclaimer}>Disclaimer</Button>
       <RightAlign>
         <Button onClick={logout}>Logout</Button>
       </RightAlign>
@@ -37,5 +38,43 @@ const Button = styled('button')({
   textTransform: 'uppercase',
   fontWeight: 600
 })
+
+const Disclaimer = ({onDismiss}) => (
+  <DWrapper>
+    <DModal>
+      Disclaimer goes here
+    </DModal>
+    <DShade onClick={onDismiss}/>
+  </DWrapper>
+)
+
+const DWrapper = styled('div')({
+  position: 'fixed',
+  width: '100%',
+  height: '100%',
+  left: 0,
+  top: 0,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: 100
+})
+
+const DModal = styled('div')({
+  background: '#fff',
+  borderRadius: 10,
+  padding: 20,
+  color: '#000'
+})
+
+const DShade = styled('div')({
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: 1
+})
+
 
 export default Footer
